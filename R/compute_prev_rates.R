7af07174d806067b1f73767ffbca9cd8fb760e51
YMao-UMCU <159454837+YMao-UMCU@users.noreply.github.com>
2026-02-02 17:20:38 +0100

    return n_ unweighted. formatting

--- R/compute_prev_rates.R
+++ R/compute_prev_rates.R
@@ -27,31 +27,44 @@ compute_prev_rates <- function(
     bootstrap,
     adjust = "adj"
 ) {
-  if (adjust == "unadj"){
-    #===== SUM PER GROUP
-    n_pat_exp <- aesifup_input[group == "EXPOSED", .N]
-    n_pat_con <- aesifup_input[group == "CONTROL", .N]
-    # sum outcomes
-    n_out_exp <- aesifup_input[group == "EXPOSED", sum(.SD[[eventCol]])]
-    n_out_con <- aesifup_input[group == "CONTROL", sum(.SD[[eventCol]])]
-  } else if (adjust == "adj") {
-    n_pat_exp <- aesifup_input[group == "EXPOSED", sum(wt)]
-    n_pat_con <- aesifup_input[group == "CONTROL", sum(wt)]
-    n_out_exp <- aesifup_input[group == "EXPOSED", sum(.SD[[eventCol]] * wt)]
-    n_out_con <- aesifup_input[group == "CONTROL", sum(.SD[[eventCol]] * wt)]
+  n_pat_exp <- aesifup_input[group == "EXPOSED", .N]
+  n_pat_con <- aesifup_input[group == "CONTROL", .N]
+  n_out_exp <- aesifup_input[group == "EXPOSED", sum(.SD[[eventCol]])]
+  n_out_con <- aesifup_input[group == "CONTROL", sum(.SD[[eventCol]])]
+  if (adjust == "unadj") {
+    n_pat_exp_weighted <- n_pat_exp
+    n_pat_con_weighted <- n_pat_con
+    n_out_exp_weighted <- n_out_exp
+    n_out_con_weighted <- n_out_con
   }
-  # point estimates
-  pp_pax <- n_out_exp / n_pat_exp
-  pp_comp <- n_out_con / n_pat_con
+  else if (adjust == "adj") {
+    n_pat_exp_weighted <- aesifup_input[group == "EXPOSED", sum(wt)]
+    n_pat_con_weighted <- aesifup_input[group == "CONTROL", sum(wt)]
+    n_out_exp_weighted <- aesifup_input[group == "EXPOSED", sum(.SD[[eventCol]] *
+                                                                  wt)]
+    n_out_con_weighted <- aesifup_input[group == "CONTROL", sum(.SD[[eventCol]] *
+                                                                  wt)]
+  }
+  pp_pax <- n_out_exp_weighted/n_pat_exp_weighted * scale_IR
+  pp_comp <- n_out_con_weighted/n_pat_con_weighted * scale_IR
   pr <- pp_pax / pp_comp
   pd <- pp_pax - pp_comp
-
-  # CI of pp (pax, unexp) or pr or pd + unweighted or weighted
-  boot_ci_list <- list()
-  for (prev_suffix in c("pp_pax", "pp_comp", "pr", "pd")) {
-    boot_col_name <- paste0(prev_suffix, "_", adjust)
-    boot_vec <- bootstrap[, get(boot_col_name)]
-    boot_ci_list[[prev_suffix]] <- est_inc_prev(scale_IR = 1, type =  "prevalence", CImethod = "bootstrap", boot_vec = boot_vec)[, .(ir_lb, ir_ub)]
+  if(is.null(bootstrap))
+    return(data.table(n_pat_exp, n_pat_con, n_out_exp, n_out_con,
+                      pp_pax, pp_comp, pr, pd))
+  else{
+    boot_ci_list <- list()
+    for (prev_suffix in c("pp_pax", "pp_comp", "pr", "pd")) {
+      boot_col_name <- paste0(prev_suffix, "_", adjust)
+      boot_vec <- bootstrap[, get(boot_col_name)]
+      boot_ci_list[[prev_suffix]] <- est_inc_prev(
+        scale_IR = 1,
+        type = "prevalence",
+        CImethod = "bootstrap",
+        boot_vec = boot_vec)[
+          , .(ir_lb, ir_ub)]
+    }
+    return(data.table(n_pat_exp, n_pat_con, n_out_exp, n_out_con,
+                      pp_pax, pp_comp, pr, pd, do.call(cbind, boot_ci_list)))
   }
-  return(data.table(n_pat_exp, n_pat_con, n_out_exp, n_out_con, pp_pax, pp_comp, pr, pd, do.call(cbind, boot_ci_list)))
 }
