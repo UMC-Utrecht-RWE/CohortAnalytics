4cdd78ef6d1475eb1f6a7c64cc6cb18d9d59ce65
YMao-UMCU <159454837+YMao-UMCU@users.noreply.github.com>
2026-03-10 19:25:59 +0100

    try again

--- R/estimate_HR.R
+++ R/estimate_HR.R
@@ -20,18 +20,16 @@ estimate_HR <- function(aesifup_input, fupCol = "fup",
                         model_type = "crude",
                         return_dummy_output = FALSE,
                         aesi_name = "",
-                        dummy_code = -88){
-
+                        dummy_code = -88) {
   dummy_output <- data.frame(hr_est = dummy_code, hr_lb = dummy_code, hr_ub = dummy_code)
-  if(return_dummy_output == TRUE){
-
+  if (return_dummy_output == TRUE) {
     return(dummy_output)
   }
 
   # check if non-zero events in both groups, return dummy output
- 
-  eventsums <- aesifup_input[, .(event_sum = sum(get(eventCol), na.rm = TRUE)), by = ..groupCol]
 
+  dt <- data.table::as.data.table(aesifup_input)
+  eventsums <- dt[, list(event_sum = sum(get(eventCol), na.rm = TRUE)), by = groupCol]
   if (nrow(eventsums) == 0 || !any(eventsums$event_sum > 0)) {
     zero_groups <- paste0(as.character(eventsums[event_sum == 0][[groupCol]]), collapse = ",")
     logger::log_info(paste0("HR model estimation fails, zero events in groups ", zero_groups))
@@ -39,31 +37,36 @@ estimate_HR <- function(aesifup_input, fupCol = "fup",
   }
 
   # fit coxmodel
-  model_formula <- as.formula(paste0("survival::Surv(",fupCol, ",",eventCol,") ~ ", groupCol))
-  modelobj <- fitmod_cox(model_formula, iptw = iptw,
-                         model_type = model_type,aesi_name = aesi_name,aesifup_input)
+  model_formula <- as.formula(paste0("survival::Surv(", fupCol, ",", eventCol, ") ~ ", groupCol))
+  modelobj <- fitmod_cox(model_formula,
+    iptw = iptw,
+    model_type = model_type, aesi_name = aesi_name, aesifup_input
+  )
 
 
   # if model fitting returned logger object or not a coxph, return dummy output
-  if(!inherits(modelobj, "coxph")){
+  if (!inherits(modelobj, "coxph")) {
     return(dummy_output)
   }
 
 
   # if model fitting error, return error flag
 
-  if(is.character(modelobj)){ return(dummy_output) }
+  if (is.character(modelobj)) {
+    return(dummy_output)
+  }
 
   # get coefficients
   coxcoef <- summary(modelobj)$coefficients
   hr_est <- coxcoef[colnames(coxcoef) == "coef"]
   hr_se <- coxcoef[colnames(coxcoef) == "robust se"]
-  if(length(hr_se) == 0)
+  if (length(hr_se) == 0) {
     hr_se <- 0
+  }
 
   # get confidence interval
-  hr_lb <- hr_est + qnorm(0.025)*hr_se
-  hr_ub <- hr_est + qnorm(0.975)*hr_se
+  hr_lb <- hr_est + qnorm(0.025) * hr_se
+  hr_ub <- hr_est + qnorm(0.975) * hr_se
 
   # exponentiate
   hr_est <- exp(hr_est)
@@ -73,4 +76,3 @@ estimate_HR <- function(aesifup_input, fupCol = "fup",
   # return object
   return(data.frame(hr_est = hr_est, hr_lb = hr_lb, hr_ub = hr_ub))
 }
-
