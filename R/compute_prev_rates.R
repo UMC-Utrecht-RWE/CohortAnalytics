1d4c478cd506e9d58140450dcca0cf3fbda3f63a
YMao-UMCU <159454837+YMao-UMCU@users.noreply.github.com>
2026-03-26 16:55:26 +0100

    fix weighted hr

--- R/compute_prev_rates.R
+++ R/compute_prev_rates.R
@@ -82,7 +82,7 @@ compute_prev_rates <- function(
       eventCol = eventCol,
       groupCol = groupCol,
       iptw = iptw,
-      model_type = "crude",
+      model_type = ifelse(adjust == "adj", "weighted", "crude"),
       return_dummy_output = FALSE,
       aesi_name = "",
       dummy_code = NA
