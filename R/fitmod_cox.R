99d94488b6c757f2ef4c4b49fd760490d1fcdb4c
YMao-UMCU <159454837+YMao-UMCU@users.noreply.github.com>
2026-03-27 10:20:36 +0100

    fix 'get' in function

--- R/fitmod_cox.R
+++ R/fitmod_cox.R
@@ -1,26 +1,35 @@
 # --- helper wrapper function to fit a cox model, return messages to log
 fitmod_cox <- function(model_formula, iptw = "ip_weights",
-                       model_type = "adj",aesi_name = NULL,aesifup_input, ...){
-  tryCatch({
-    if(model_type == "crude"){
-      return(survival::coxph(model_formula,
-                             cluster = person_id, id = person_id,
-                             robust = T, data = aesifup_input)
-      )
+                       model_type = "adj", aesi_name = NULL, aesifup_input, ...) {
+  tryCatch(
+    {
+      if (model_type == "crude") {
+        return(survival::coxph(model_formula,
+          cluster = person_id, id = person_id,
+          robust = TRUE, data = aesifup_input
+        ))
+      }
+      if (model_type == "adj") {
+        w <- aesifup_input[[iptw]]
+        pid <- aesifup_input[["person_id"]]
+
+        return(survival::coxph(
+          model_formula,
+          data = aesifup_input,
+          robust = TRUE,
+          id = pid,
+          cluster = pid,
+          weights = w
+        ))
+      }
+    },
+    error = function(cond) {
+      logger::log_info(paste("Cox model fitting returning error for ", model_type, " model, ", aesi_name))
+      logger::log_info(conditionMessage(cond))
+    },
+    warning = function(cond) {
+      logger::log_info(paste("Cox model fitting returning warning for ", model_type, " model, ", aesi_name))
+      logger::log_info(conditionMessage(cond))
     }
-    if(model_type == "adj"){
-      return(survival::coxph(model_formula,
-                             cluster = person_id, id = person_id,
-                             robust = T, data = aesifup_input,
-                             weights = get(iptw)))
-    }
-  },
-  error = function(cond){
-    logger::log_info(paste("Cox model fitting returning error for ",model_type, " model, ", aesi_name))
-    logger::log_info(conditionMessage(cond))
-  },
-  warning = function(cond){
-    logger::log_info(paste("Cox model fitting returning warning for ",model_type, " model, ", aesi_name))
-    logger::log_info(conditionMessage(cond))
-  }
-  )}
+  )
+}
