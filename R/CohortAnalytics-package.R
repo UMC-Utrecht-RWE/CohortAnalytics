535620bb4fac72189bc13762b2ca7f7a80d63ef8
Solon Ioannou <36792048+ioannousolon@users.noreply.github.com>
2026-09-17 18:56:54 +0200

    adding package level documentation

---
+++ R/CohortAnalytics-package.R
@@ -0,0 +1,20 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom data.table :=
+#' @importFrom data.table .N
+#' @importFrom data.table .SD
+## usethis namespace: end
+NULL
+
+utils::globalVariables(c(
+  "group",
+  "aesifup",
+  "person_id",
+  "V1",
+  "person_id_num",
+  "id",
+  "pyr_offset",
+  "target_aesi"
+))
